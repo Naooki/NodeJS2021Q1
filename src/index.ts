75fe97f1094a8baa68000ac0a4eeced6a4b8ac2f
Yahor Sheinikau <yahor_sheinikau@epam.com>
2021-03-02 19:13:53 +0300

    Implement task 1.1.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('hello world!');
+import { Transform, TransformCallback } from 'stream';
+
+class ReverseStream extends Transform {
+  _transform(
+    chunk: Buffer,
+    encoding: BufferEncoding,
+    callback: TransformCallback,
+  ) {
+    const reversed = chunk
+      .toString('utf8')
+      .split('')
+      .slice(0, -1) // remove extra new-line
+      .reverse()
+      .join('');
+    callback(null, `${reversed}\n\n`);
+  }
+}
+
+const reverseStream = new ReverseStream();
+
+process.stdin.pipe(reverseStream).pipe(process.stdout);
