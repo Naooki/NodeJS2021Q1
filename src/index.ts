c22d0439649d25a3b5c093fdf51afdde9cd9965c
Yahor Sheinikau <yahor_sheinikau@epam.com>
2021-03-20 19:18:30 +0300

    feat: Add user routes (crud operations)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express, { Router } from 'express';
 
 import { statusRoutes } from './routes/status';
+import { usersRoutes } from './routes/users';
 
 const app = express();
 
+app.use(express.json());
+
 const routes: { [key: string]: Router } = {
   '': statusRoutes,
+  'users': usersRoutes,
 };
 
 Object.keys(routes).forEach((key: string) => {
