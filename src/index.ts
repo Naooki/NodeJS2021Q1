117dc1b6d2d76304d42a004bc1a55c03806439ae
Yahor Sheinikau <yahor_sheinikau@epam.com>
2021-05-11 21:42:17 +0300

    feat: Add uncughtException and unhandledRejection logging.

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,17 @@ import express from 'express';
 
 import { initContainer } from './infrastructure/inversify.config';
 import { TOKENS } from './infrastructure/tokens';
+import { Logger } from './infrastructure/logger';
 import './routes/status';
 import './routes/user';
 import './routes/group';
 
 initContainer().then((container) => {
   const server = new InversifyExpressServer(container);
+
+  const logger = container.get<Logger>(TOKENS.Logger);
+  setupUnhandledRejectionListener(logger);
+  setupUncaughtExceptionListener(logger);
 
   server
     .setConfig((app) => {
@@ -31,3 +36,21 @@ initContainer().then((container) => {
     .build()
     .listen(3000, () => console.log('Server is listening on port 3000!'));
 });
+
+function setupUnhandledRejectionListener(logger: Logger) {
+  process.on('unhandledRejection', (reason) =>
+    logger.log({
+      level: 'error',
+      message: `Unhandled promise rejection: ${reason}`,
+    }),
+  );
+}
+
+function setupUncaughtExceptionListener(logger: Logger) {
+  process.on('uncaughtException', (err) =>
+    logger.log({
+      level: 'error',
+      message: `Unhandled exception: ${err}`,
+    }),
+  );
+}
